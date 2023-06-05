3fd96a5a751029eda9159c06ed36e3fc9f84d7eb
zcyi <zaoyiii@outlook.com>
2023-06-05 12:45:33 +0800

    调试震动

--- app/src/main/java/com/zcyi/rorschach/Pager/Fragment/AlarmMePagerFragment.java
+++ app/src/main/java/com/zcyi/rorschach/Pager/Fragment/AlarmMePagerFragment.java
@@ -1,6 +1,9 @@
 package com.zcyi.rorschach.Pager.Fragment;
 
 
+import static android.app.PendingIntent.FLAG_IMMUTABLE;
+import static com.zcyi.rorschach.Util.Constant.TAG;
+
 import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
@@ -15,9 +18,7 @@ import android.media.AudioAttributes;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.provider.Settings;
@@ -35,7 +36,6 @@ import androidx.fragment.app.Fragment;
 
 import com.zcyi.rorschach.MainActivity;
 import com.zcyi.rorschach.R;
-import com.zcyi.rorschach.Util.Constant;
 
 import java.util.Objects;
 
@@ -68,31 +68,34 @@ public class AlarmMePagerFragment extends Fragment {
         public void onReceive(Context context, Intent intent) {
             //定时任务
             //todo
-            Log.e("zcyi", "onReceive: 执行保存历史数据");
-
-            //如果版本高于4.4，需要重新设置闹钟
-
-            alarmMgr.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + 1000 * 5, alarmIntent);
+            Log.e("zcyi", "onReceive: Notification");
+            Notification();
 
         }
     };
 
     public void alarmClockTest() {
-        Log.e(Constant.TAG, "alarmClockTest: 调用" );
+        Log.e(TAG, "alarmClockTest: 调用");
+        Intent intent = new Intent();
+        intent.setAction(ALARM_ACTION);
         IntentFilter intentFilter = new IntentFilter(ALARM_ACTION);
         v.getContext().registerReceiver(alarmReceiver, intentFilter);
-        alarmMgr = (AlarmManager) v.getContext().getSystemService(Context.ALARM_SERVICE);
+        PendingIntent sender;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.S) {
+            sender = PendingIntent.getBroadcast(v.getContext(), 0, intent, PendingIntent.FLAG_IMMUTABLE);
 
-        alarmIntent = PendingIntent.getBroadcast(v.getContext(), 0, new Intent(ALARM_ACTION), 0);
-        alarmMgr.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                SystemClock.elapsedRealtime() +
-                        5 * 1000, alarmIntent);
+        } else {
+            sender = PendingIntent.getBroadcast(v.getContext(), 0, intent, 0);
+
+        }
+
+        AlarmManager alarm = (AlarmManager) v.getContext().getSystemService(Context.ALARM_SERVICE);
+        alarm.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5 * 1000,0, sender);
     }
 
     public void Notification() {
         manager = (NotificationManager) Objects.requireNonNull(getActivity()).getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationChannel channel = null;
-        channel = new NotificationChannel("zyci", "李在赣神魔", NotificationManager.IMPORTANCE_HIGH);
+        NotificationChannel channel = new NotificationChannel("zyci", "李在赣神魔", NotificationManager.IMPORTANCE_HIGH);
         channel.enableLights(true);
         channel.enableVibration(true);
 
@@ -103,7 +106,7 @@ public class AlarmMePagerFragment extends Fragment {
         channel.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION), audioAttributes);
         manager.createNotificationChannel(channel);
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, new Intent(getContext(), MainActivity.class), PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, new Intent(getContext(), MainActivity.class), FLAG_IMMUTABLE);
         notification = new NotificationCompat.Builder(getContext(), "zyci")
                 .setContentTitle("在干嘛")
                 .setContentText("到点了，该吃药了~大朗")
@@ -136,7 +139,13 @@ public class AlarmMePagerFragment extends Fragment {
      */
     private static void playNotificationVibrate(Context context) {
         Vibrator vibrator = (Vibrator) context.getSystemService(Service.VIBRATOR_SERVICE);
-        vibrator.vibrate(VibrationEffect.createOneShot(5000, 10));
+        AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                .setUsage(AudioAttributes.USAGE_ALARM) //key
+                .build();
+        long[] times = {0, 2000};
+        vibrator.vibrate(VibrationEffect.createWaveform(times, -1));
+        Log.e(TAG, "playNotificationVibrate: 执行震动");
     }
 
 }
